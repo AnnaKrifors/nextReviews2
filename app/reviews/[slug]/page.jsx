39940c3d791f9ab71e310884f669b1fb90bf6823
AnnaKrifors <annsisen1@gmail.com>
2024-09-26 09:32:47 +0200

    added from course

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,11 +1,11 @@
-import Heading from '@/components/Heading';
-import ShareLinkButton from '@/components/ShareLinkButton';
-import { getReview, getSlugs } from '@/lib/reviews';
+import Heading from "@/components/Heading";
+import ShareLinkButton from "@/components/ShareLinkButton";
+import { getReview, getSlugs } from "@/lib/reviews";
 
-export async function generateStaticParams() {
-  const slugs = await getSlugs();
-  return slugs.map((slug) => ({ slug }));
-}
+// export async function generateStaticParams() {
+//   const slugs = await getSlugs();
+//   return slugs.map((slug) => ({ slug }));
+// }
 
 export async function generateMetadata({ params: { slug } }) {
   const review = await getReview(slug);
@@ -16,7 +16,7 @@ export async function generateMetadata({ params: { slug } }) {
 
 export default async function ReviewPage({ params: { slug } }) {
   const review = await getReview(slug);
-  console.log('[ReviewPage] rendering', slug);
+  console.log("[ReviewPage] rendering", review);
   return (
     <>
       <Heading>{review.title}</Heading>
@@ -24,12 +24,8 @@ export default async function ReviewPage({ params: { slug } }) {
         <p className="italic pb-2">{review.date}</p>
         <ShareLinkButton />
       </div>
-      <img src={review.image} alt=""
-        width="640" height="360" className="mb-2 rounded"
-      />
-      <article dangerouslySetInnerHTML={{ __html: review.body }}
-        className="max-w-screen-sm prose prose-slate"
-      />
+      <img src={review.image} alt="" width="640" height="360" className="mb-2 rounded" />
+      <article dangerouslySetInnerHTML={{ __html: review.body }} className="max-w-screen-sm prose prose-slate" />
     </>
   );
 }
